43eb7ddfc33bc5fd83e114a2be23cb47135876e9
shankha117 <sinha.shankha17@gmail.com>
2024-02-24 01:02:12 +0530

    constants and primitives

---
+++ Prac_2024/constant.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+const a = "abcd"
+
+func main() {
+
+	// const myConstat float64 = math.Sin(1.57)
+	// fmt.Println(myConstat)
+	fmt.Printf(" %v, %T \n ", a, a)
+
+	const a = 123
+	fmt.Printf(" %v, %T \n ", a, a)
+
+	const e = 42
+	// var c = 23
+	var b int16 = 21
+
+	fmt.Printf("%v , %T \n", e, e)
+	fmt.Printf("%v , %T \n", e+b, e+b)
+	// fmt.Println("%v , %T", c+b, c+b)
+}
--- Prac_2024/primitives.go
+++ Prac_2024/primitives.go
@@ -1,11 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func Prims() {
 	fmt.Println("THIS IS COOL")
 }
 
 func main() {
-	Prims() // Call Prims() function inside the main function
+
+	var k bool = false
+
+	fmt.Printf(" %v , %T \n", k, k)
+
+	n := 1 == 1
+	m := 2 == 2
+
+	fmt.Printf("%v , %T\n", m, m)
+	fmt.Printf("%v , %T\n", n, n)
+
+	a, b := 10, 3
+
+	// a -> 1010
+	// b -> 0011
+
+	fmt.Println(a & b)  // 0010
+	fmt.Println(a | b)  // 1011
+	fmt.Println(a ^ b)  // 1001
+	fmt.Println(a &^ b) // 0100
+
+	s := "This is me"
+	fmt.Println(s, reflect.TypeOf(s))
+	fmt.Println(string(s[2]))
+	fmt.Printf("%v , %T\n", s[2], s[2])
+
+	// d := "This is me"
+	// d[2] = 'k'
+
+	sb := []byte(s)
+	// list ascii values or the utf values in the string `s`
+	fmt.Println(reflect.TypeOf(sb), sb, string(sb))
+
+	// RUNES
+
+	r := 'a'
+	fmt.Printf(" %v, %T \n", r, r)
+
+	rs := "a"
+	fmt.Printf(" %v, %T \n", rs, rs)
+
+	// var ru rune = "a"
+	// fmt.Printf(" %v, %T \n", ru, ru)
+
 }
